159fb39bbe979657f8fc27876cde2d27cdcf28bf
sund3RRR <evenquantity@gmail.com>
2025-03-19 16:15:00 +0300

    code-cursor: overlay

--- pkgs/generic/cursor-s/default.nix
+++ pkgs/generic/cursor-s/default.nix
@@ -23,14 +23,6 @@ let
       url = "https://download.todesktop.com/230313mzl4w4u92/cursor-0.45.14-build-250219jnihavxsz-arm64.AppImage";
       hash = "sha256-8OUlPuPNgqbGe2x7gG+m3n3u6UDvgnVekkjJ08pVORs=";
     };
-    x86_64-darwin = fetchurl {
-      url = "https://download.todesktop.com/230313mzl4w4u92/Cursor%200.45.14%20-%20Build%20250219jnihavxsz-x64.dmg";
-      hash = "sha256-NyDY74PZjSjpuTSVaO/l9adPcLX1kytyrFGQjJ/8WcQ=";
-    };
-    aarch64-darwin = fetchurl {
-      url = "https://download.todesktop.com/230313mzl4w4u92/Cursor%200.45.14%20-%20Build%20250219jnihavxsz-arm64.dmg";
-      hash = "sha256-A503TxDDFENqMnc1hy/lMMyIgC7YwwRYPJy+tp649Eg=";
-    };
   };
 
   source = sources.${hostPlatform.system};
@@ -45,59 +37,24 @@ let
         -exec sed -i 's/,minHeight/,frame:false,minHeight/g' {} \;
     '');
   };
-
-  wrappedAppimage = appimageTools.wrapAppImage {
-    inherit version pname;
-    src = appimageContents;
-  };
 in
-stdenvNoCC.mkDerivation {
+appimageTools.wrapAppImage {
   inherit pname version;
 
-  src = if hostPlatform.isLinux then wrappedAppimage else source;
+  src = appimageContents;
 
-  nativeBuildInputs =
-    lib.optionals hostPlatform.isLinux [ makeWrapper ]
-    ++ lib.optionals hostPlatform.isDarwin [ undmg ];
+  nativeBuildInputs = [ makeWrapper ];
 
-  sourceRoot = lib.optionalString hostPlatform.isDarwin ".";
+  extraInstallCommands = ''
+    mkdir -p $out/share/cursor $out/share/applications/
+    cp -a ${appimageContents}/locales $out/share/cursor
+    cp -a ${appimageContents}/usr/share/icons $out/share/
+    install -Dm 644 ${appimageContents}/cursor.desktop -t $out/share/applications/
 
-  # Don't break code signing
-  dontUpdateAutotoolsGnuConfigScripts = hostPlatform.isDarwin;
-  dontConfigure = hostPlatform.isDarwin;
-  dontFixup = hostPlatform.isDarwin;
+    substituteInPlace $out/share/applications/cursor.desktop --replace-fail "AppRun" "cursor"
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/
-
-    ${lib.optionalString hostPlatform.isLinux ''
-      cp -r bin $out/bin
-      mkdir -p $out/share/cursor
-
-      cp -a ${appimageContents}/locales $out/share/cursor
-      cp -a ${appimageContents}/usr/share/icons $out/share/
-      install -Dm 644 ${appimageContents}/cursor.desktop -t $out/share/applications/
-      substituteInPlace $out/share/applications/cursor.desktop --replace-fail "AppRun" "cursor"
-
-      wrapProgram $out/bin/cursor \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}} --no-update"
-    ''}
-
-    ${lib.optionalString hostPlatform.isDarwin ''
-      APP_DIR="$out/Applications"
-      CURSOR_APP="$APP_DIR/Cursor.app"
-      mkdir -p "$APP_DIR"
-      cp -Rp Cursor.app "$APP_DIR"
-      mkdir -p "$out/bin"
-      cat << EOF > "$out/bin/cursor"
-      #!${stdenvNoCC.shell}
-      open -na "$CURSOR_APP" --args "\$@"
-      EOF
-      chmod +x "$out/bin/cursor"
-    ''}
-
-    runHook postInstall
+    wrapProgram $out/bin/cursor \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}} --no-update"
   '';
 
   passthru = {
--- sunderBook/configuration.nix
+++ sunderBook/configuration.nix
@@ -4,6 +4,7 @@
 
 {
   pkgs,
+  config,
   lib,
   ...
 }:
@@ -25,6 +26,44 @@
   nixld.enable = true;
   zapret.enable = true;
   gaming.enable = true;
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      code-cursor = (
+        let
+          appimageContents = prev.appimageTools.extractType2 {
+            inherit (prev.code-cursor) version pname;
+            src = prev.code-cursor.sources.${config.nixpkgs.hostPlatform.system};
+            postExtract = ''
+              find $out -type f -name '*.js' \
+                -exec grep -l ,minHeight {} \; \
+                -exec sed -i 's/,minHeight/,frame:false,minHeight/g' {} \;
+            '';
+          };
+        in
+        prev.appimageTools.wrapAppImage {
+          inherit (prev.code-cursor) pname version;
+          src = appimageContents;
+
+          nativeBuildInputs = [ prev.makeWrapper ];
+
+          extraInstallCommands = ''
+            mkdir -p $out/share/cursor $out/share/applications/
+            cp -a ${appimageContents}/locales $out/share/cursor
+            cp -a ${appimageContents}/usr/share/icons $out/share/
+            install -Dm 644 ${appimageContents}/cursor.desktop -t $out/share/applications/
+
+            substituteInPlace $out/share/applications/cursor.desktop --replace-fail "AppRun" "cursor"
+
+            wrapProgram $out/bin/cursor \
+              --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}} --no-update"
+          '';
+
+          passthru = prev.code-cursor.passthru;
+        }
+      );
+    })
+  ];
 
   qt = {
     enable = true;
@@ -61,7 +100,7 @@
     layout = "us";
     variant = "";
   };
-  
+
   security.rtkit.enable = true;
 
   users.users.sunder = {
@@ -106,7 +145,7 @@
     lact
     prismlauncher
     zed-editor
-    cursor-s
+    code-cursor
 
     # Dependencies
     adwaita-qt6 # for window decorations

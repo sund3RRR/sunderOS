b5b3de8585db8ced4e6bf4c97ccfd614fddfb307
sund3RRR <evenquantity@gmail.com>
2025-04-07 18:53:19 +0300

    feat: update sunderPC

--- hosts/sunderPC/configuration.nix
+++ hosts/sunderPC/configuration.nix
@@ -1,54 +1,38 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# MECHREVO WUJIE-14X
 {
   pkgs,
-  unstable,
+  username,
   ...
 }:
 {
   imports = [
-    ./boot.nix
-    ./filesystems.nix
     ./hardware.nix
-    ./networking.nix
-    ./nix-config.nix
-    ./virtualisation.nix
+
+    ../general/boot.nix
+    ../general/cursor-overlay.nix
+    ../general/gnome.nix
+    ../general/networking.nix
+    ../general/nix-config.nix
+    ../general/prismlauncher-overlay.nix
+    ../general/programs.nix
+    ../general/services.nix
+    ../general/virtualisation.nix
   ];
 
-  programs.gaming.enable = true;
-  programs.amnezia-vpn.enable = true;
-  programs.zsh = {
-    enable = true;
-    syntaxHighlighting.enable = true;
-    autosuggestions.enable = true;
-    zsh-autoenv.enable = true;
-    shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake ~/sunderOS#sunderPC";
-    };
-  };
-  programs.dconf.enable = true;
+  nixld.enable = true;
+  zapret.enable = true;
+  gaming.enable = true;
 
-  services.flatpak.enable = true;
-  services.avahi.enable = true;
+  programs.hyprland.enable = true;
+
+  qt = {
+    enable = true;
+  };
 
   xdg.portal = {
     enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.kdePackages.xdg-desktop-portal-kde
-    ];
     xdgOpenUsePortal = true;
   };
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
 
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
@@ -68,105 +52,72 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.desktopManager.cosmic.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
-  #services.displayManager.sddm.enable = true;
-  #services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sunder = {
+  users.users.${username} = {
     isNormalUser = true;
-    description = "sunder";
+    description = username;
     extraGroups = [
       "networkmanager"
       "wheel"
-      "docker"
-      "libvirtd"
     ];
     shell = pkgs.zsh;
-    packages = with pkgs; [
-      kdePackages.kate
-      telegram-desktop
-      #  thunderbird
-    ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.firefox.nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
-
-  # Allow unfree packages
-
   fonts.packages = with pkgs; [ meslo-lgs-nf ];
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
+  environment.variables = {
+    #NIXOS_OZONE_WL = "1";
+  };
+
   environment.systemPackages = with pkgs; [
+    # Terminal
+    amdgpu_top
+    btop
+    curl
     distrobox
-    vscode
+    fastfetch
+    git
+    htop
     tree
     nixfmt-rfc-style
-    unstable.wineWowPackages.stagingFull
-    firefoxpwa
-    pods
-    unstable.nix-update
+    micro
+    s-tui
+    wget
+    python3
+
+    # Desktop apps
+    code-cursor
+    vscode
+    lact
+    mailspring
+    zed-editor
+    prismlauncher
+
+    # Dependencies
+    adwaita-qt6 # for window decorations
+    ddcutil # for brightness extension
+    firefoxpwa # for firefox pwa
+    wl-clipboard # for micro
+    nautilus-python # for collision nautilus extension
+    zenity # for mailspring notifications
+
+    # Themes
+    adw-gtk3
+    andromeda-gtk-theme # bug
+    colloid-gtk-theme
+    fluent-gtk-theme # bug
+    vimix-gtk-themes
+    tokyonight-gtk-theme
+    lavanda-gtk-theme
+    matcha-gtk-theme
+    whitesur-gtk-theme # bug
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
